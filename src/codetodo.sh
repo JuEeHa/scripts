90d969613c35324341b49ec561f8c22327c0f5f4
Juhani Haverinen <juhani.haverinen@gmail.com>
2013-11-21 19:05:33 +0200

    Added codetodo.sh

---
+++ src/codetodo.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+gettodo() {
+	grep '/\* TODO' $1*.c $1*.h 2> /dev/null
+	grep '# TODO' $1*.sh 2> /dev/null
+}
+
+if test z"$1" = z""
+then
+	gettodo
+else
+	SDIR=$(pwd)
+	for i in "$@"
+	do
+		if test -d "$i"
+		then
+			gettodo $i/
+		fi
+	done
+fi
