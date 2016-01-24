6f9e54096400b6b75f00abcc8efa2a1efd772aec
Juhani Haverinen <juhani.haverinen@gmail.com>
2016-01-24 17:22:27 +0200

    Add ssh public key support to backup.sh

--- src/backup.sh
+++ src/backup.sh
@@ -29,11 +29,25 @@ remote_scp() {
 	echo "scp *.txz ${prefix}_sha512_$timestamp $1/ || exit 1" >> $tmpdir/cmd.sh
 }
 
+remote_scp_pubkey() {
+	echo "echo 'Copying (scp) to $(echo $1 | cut -d : -f 1)'" >> $tmpdir/cmd.sh
+	echo "scp -i '$2' *.txz ${prefix}_sha512_$timestamp $1/ || exit 1" >> $tmpdir/cmd.sh
+}
+
 remote_ssh() {
 	host="$1"
 	shift
 	echo "echo 'Executing commands on $host'" >> $tmpdir/cmd.sh
 	echo "ssh $host $@ || exit 1" >> $tmpdir/cmd.sh
+}
+
+remote_ssh_pubkey() {
+	host="$1"
+	shift
+	key="$1"
+	shift
+	echo "echo 'Executing commands on $host'" >> $tmpdir/cmd.sh
+	echo "ssh -i '$key' $host $@ || exit 1" >> $tmpdir/cmd.sh
 }
 
 local_cp() {
